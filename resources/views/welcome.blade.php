8f0651cfa529b5bdfebbdb608ec7f24913ca9ec4
Adonis Ventocilla Sanchez <aventocillas@autonoma.edu.pe>
2022-11-09 12:07:03 -0500

    Add API item

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
-    $('#category').select2({
+    $('#caegory').select2({
         tags: true,
         tokenSeparators: [','],
         ajax: {
